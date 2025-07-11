0771f35d82efd442e6a869fdc3062c7b00054bcd
Harsh-Mahatha <harshmahatha1847@gmail.com>
2025-07-11 15:07:28 +0530

    Movement Done

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float moveSpeed = 5f;
+    public PlayerInputActions inputActions;
+
+    public Animator anim;
+
+    private void OnEnable()
     {
-        
+        if (inputActions == null)   
+            inputActions = new PlayerInputActions();
+        inputActions.Enable();
     }
 
-    // Update is called once per frame
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
     void Update()
     {
-        
+        Vector2 moveInput = inputActions.Player.Move.ReadValue<Vector2>();
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+        transform.Translate(move * moveSpeed * Time.deltaTime, Space.World);
+
+        anim.SetFloat("MoveX", moveInput.x, 0.1f, Time.deltaTime);
+        anim.SetFloat("MoveY", moveInput.y, 0.1f, Time.deltaTime);
+
     }
 }
